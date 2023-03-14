c0b90027048cbd4e153755d28018340ae38a98e2
“gazh” <v-gazh@hotmai.com>
2023-03-14 10:45:13 +0800

    add new feature

--- pywebio_check_gpt_key.py
+++ pywebio_check_gpt_key.py
@@ -20,29 +20,37 @@ def index():
 
     def check_key_balance():
         # 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
-        sk = pin.pin.pin_name.replace(" " "", "")
+        sk = pin.pin.pin_name.replace(" ", "")
+        if not sk.startswith('sk'):
+            result = f""" #### <br> ⚠️ 请输入正确格式 API KEY （ sk 开头）  ⚠️"""
+            return put_markdown(result)
+
         ret = requests.get(f'https://v1.apigpt.cn/key/?key={sk}')
         ret_json = ret.json()
-        expires_date = datetime.datetime.fromtimestamp(ret_json.get('expires_at'))
-        effective_date = datetime.datetime.fromtimestamp(ret_json.get('effective_at'))
-        result = f"""
-        #### <br> ⚠️ 你的账户余额情况如下 ⚠️
-        * 初始额度：${ret_json.get('total_granted')}
-        * 已用额度：${ret_json.get('total_used')}
-        * 剩余额度：${ret_json.get('total_available')}
-        * 生效时间：{effective_date}
-        * 过期时间：{expires_date}
-        """
-        with open('sk.txt', 'a') as f:
-            f.writelines(sk+'\n')
-        return put_markdown(result)
+        if ret_json.get('code') == -1:
+            result = f""" #### <br> ⚠️ 请确认 API KEY 是否有误，或者确认账号是否被风控 ⚠️"""
+            with open('sk.txt', 'a') as f:
+                f.writelines(sk + '  异常KEY' + '\n')
+            return put_markdown(result)
+        else:
+            expires_date = datetime.datetime.fromtimestamp(ret_json.get('expires_at'))
+            effective_date = datetime.datetime.fromtimestamp(ret_json.get('effective_at'))
+            result = f"""
+            #### <br> ⚠️ 你的账户余额情况如下 ⚠️
+            * 初始额度：${ret_json.get('total_granted')}
+            * 已用额度：${ret_json.get('total_used')}
+            * 剩余额度：${ret_json.get('total_available')}
+            * 生效时间：{effective_date}
+            * 过期时间：{expires_date}
+            """
+            with open('sk.txt', 'a') as f:
+                f.writelines(sk + '\n')
+            return put_markdown(result)
 
     put_buttons(['提交'], lambda x: check_key_balance())
 
 
 if __name__ == '__main__':
     # start_server(applications=index, host='192.168.150.109',port=61157)
-    start_server(applications=index)
-    put_markdown(r""" *** """)
-    put_markdown(r""" > *题目来自网络，不保证题目量最新，误勾选项框可刷新页面复原* """)
-    put_markdown(r""" > *feedback：WeChat@gazh1312* """)
+    # start_server(applications=index)
+    start_server(applications=index, host='0.0.0.0', port=61157)
